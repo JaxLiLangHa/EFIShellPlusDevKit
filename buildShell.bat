ee1403ae3f088e7c7bf3a73e5e0b4f925f5e34aa
JaxLiLangHa <liyong4070@126.com>
2018-10-31 16:36:48 +0800

    ignore file "ShellX64.efi".

--- buildShell.bat
+++ buildShell.bat
@@ -1,4 +1,19 @@
 @echo off
+
+if /I "%1"=="-?" (
+    goto help
+)
+
+if /I "%1"=="clean" (
+    goto clean
+)
+
+if /I "%1"=="all" (
+    goto clean
+)
+
+:buildproject
+
 set WORKSPACE=%CD%
 set IASL_PREFIX=%WORKSPACE%\Tools\
 set NASM_PREFIX=%WORKSPACE%\Tools\
@@ -12,6 +27,32 @@ set TARGET=RELEASE
 set TARGET_ARCH=X64
 set TOOL_CHAIN_TAG=VS2015x86
 
-@echo on
-@ call build -p %ACTIVE_PLATFORM% -a %TARGET_ARCH% -b %TARGET% -t %TOOL_CHAIN_TAG%
-xcopy /y %WORKSPACE%\Build\EdkShellPkg\%TARGET%_%TOOL_CHAIN_TAG%\%TARGET_ARCH%\ShellFull.efi .\ShellX64.efi
+call build -p %ACTIVE_PLATFORM% -a %TARGET_ARCH% -b %TARGET% -t %TOOL_CHAIN_TAG%
+if %errorlevel% NEQ 0 goto end
+xcopy /y %WORKSPACE%\Build\EdkShellPkg\%TARGET%_%TOOL_CHAIN_TAG%\%TARGET_ARCH%\Shell.efi .\ShellX64.efi
+goto end
+
+:help
+echo ===========================================================
+echo buildshell.bat [Arg1]:
+echo    Arg1: all   -- full build
+echo         clean -- delete Build/*.*
+echo ===========================================================
+goto:eof
+
+:clean
+if exist Build (
+        del /s /q /f Build\*.*
+        rd /q /s Build\
+)
+
+if /I "%1"=="all" (
+    goto buildproject
+) else (
+    goto end
+)
+goto:eof
+
+:end
+REM
+goto:eof
